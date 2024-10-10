6af5cad64c2fceec3268e9ffd8392910f80e6efe
Gabodze <mgabodze96@gmail.com>
2024-10-11 00:18:34 +0400

    Update src/index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+// numbers array
+const numArray = [10, 20, 30, 40, 50];
+
+function Numbers(props){
+  return(
+      <h2>{props.numbers.join(', ')}</h2>
+  );
+}
+
+
+
+
+
+
+
+var root = document.getElementById("root");
+ReactDOM.createRoot(root).render(<Numbers numbers={numArray}/>);
